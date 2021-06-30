e684ae3e3c929fd5ab64d34e6c701c7b40f6cb19
Carolina Cappato <carolinacappato@gmail.com>
2021-06-30 03:18:59 -0300

    Tests

--- src/test/java/com/biblioteca/big/controller/UserControllerTest.java
+++ src/test/java/com/biblioteca/big/controller/UserControllerTest.java
@@ -1,31 +1,33 @@
 package com.biblioteca.big.controller;
 
-import com.biblioteca.big.repository.UserRepository;
-import com.biblioteca.big.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
+import com.biblioteca.big.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
-    private UserRepository userRepository;
-
-    private UserService userServiceUnderTest;
-
-    @BeforeEach
-    void setUp(){
-        userServiceUnderTest = new UserService(userRepository);
-    }
+    @Autowired
+    private UserController userController;
 
     @Test
     @DisplayName("It should add a given user in database")
-    void insertUserTest() {
+    void insertUserTest() throws Exception {
+        User user = new User("John", "Doe", 11222333L ,"johndoe@gmail.com");
+
+        given(userController.newUser(user)).willReturn(ResponseEntity.status(201).build());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users"));
     }
 }
